ee1c4a2af47e2c3ae21dac5b12e048b8a731bedb
Mishin870 <polsit@yandex.ru>
2017-10-27 19:13:37 +0400

    recurrent generator + statistics

---
+++ main.py
@@ -0,0 +1,29 @@
+import random
+
+class RecurrentGenerator():
+
+    def __init__(self, A, C, M):
+        self.A = A
+        self.C = C
+        self.M = M
+        self.prev_val = random.random()
+
+    def generate_next(self):
+        self.prev_val = (self.prev_val * self.A + self.C) % self.M
+        return self.prev_val
+
+if __name__ == '__main__':
+    #A = input('Введите A:')
+    #C = input('Введите C:')
+    #gen = RecurrentGenerator(A, C, 8)
+    gen = RecurrentGenerator(1, 1, 8)
+    for n in range(5):
+        hits = [0 for x in range(9)]
+        for i in range(200):
+            val = gen.generate_next() / 8
+            for h in range(1, 10):
+                if val > h / 10:
+                    break
+                else:
+                    hits[h - 1] += 1
+        print(hits)
